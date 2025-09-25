0a0eab09fa2b294f181648e9607d04818ead658d
looking oddly good <132559280+Megach-ffs@users.noreply.github.com>
2025-09-25 20:24:13 +0500

    Create h1.py

---
+++ lesson-1/homework/h1.py
@@ -0,0 +1,44 @@
+
+# Task 1
+a = int(input("Enter a number: "))
+
+perimeter = a*4
+wow = a**2
+print(wow)
+print("Perimeter: " + str(perimeter))
+print("Area:", wow)
+
+
+# Task 2
+
+d = int(input("Enter a number: "))
+length = 3.14*d
+
+print("Length:", length)
+
+# Task 3
+
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+
+mean = (a + b)/2
+print("Mean:", mean)
+
+# Task 4
+
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+
+sum1 = a + b
+product = a * b
+a_2 = a**2
+b_2 = b**2
+
+print("Sum:", sum1)
+print("Product:", a*b)
+print("a square:", a_2)
+print("b square:",b_2)
+
+
+
+
