47b4a6ac43dc311b1e2698378c40aefc04088f38
looking oddly good <132559280+Megach-ffs@users.noreply.github.com>
2025-09-25 20:25:09 +0500

    Create h2.py

---
+++ lesson-2/homework/h2.py
@@ -0,0 +1,56 @@
+# Task 1
+
+name = input("What is your name: ")
+year = int(input("Which year u born: "))
+
+print(name, "Age:", 2025-year)
+
+
+# Task 2
+
+txt = 'LMaasleitbtui'
+print(txt[0:2:1])
+
+
+# Task 3
+txt = 'MsaatmiazD'
+print(txt[0:5:1])
+
+# Task 4
+txt = "I'am John. I am from London"
+
+print(txt[21:])
+
+# Task 5
+
+txt = input("text: ")
+print(txt[::-1])
+
+# Task 6
+
+txt = input("text: ")
+txt = txt.lower()
+print(txt.count("a")+txt.count("e")+txt.count("u")+txt.count("i")+txt.count("o"))
+
+
+# Task 7
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+print(max(a,b,c))
+
+# Task 8
+
+txt = input("Enter txt: ")
+print(txt == txt[::-1])
+
+# Task 9
+
+email = input("Enter email ")
+start = email.find("@")
+print(email[start+1:])
+
+# Task 10
+# This topic wasnt covered yet -_-
